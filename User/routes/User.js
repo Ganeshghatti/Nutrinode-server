5a615be07bf7fe1f980e06f6222943f66839585f
Ganeshghatti <ghattiganesh8@gmail.com>
2024-04-29 00:33:57 +0530

    removed middleware

--- User/routes/User.js
+++ User/routes/User.js
@@ -16,12 +16,12 @@ const requireAuth = require("../middleware/User");
 
 router.route("/user/register").post(register);
 router.route("/user/login").post(login);
-router.route("/user/my-account").get(requireAuth,MyAccount);
-router.route("/user/my-account").put(requireAuth,EditAccount);
-router.route("/user/add-to-diet").post(requireAuth,AddToDiet);
-router.route("/user/detect-food").post(requireAuth,DetectFood);
-router.route("/user/chat").post(requireAuth,Chat);
-router.route("/user/get-diet").get(requireAuth,GetDiet);
+router.route("/user/my-account").get(MyAccount);
+router.route("/user/my-account").put(EditAccount);
+router.route("/user/add-to-diet").post(AddToDiet);
+router.route("/user/detect-food").post(DetectFood);
+router.route("/user/chat").post(Chat);
+router.route("/user/get-diet").get(GetDiet);
 router.route("/user/forgot-password").post(ForgotPassword)
 router.route("/user/reset-password/:resetPasswordToken").post(ResetPassword);
 
